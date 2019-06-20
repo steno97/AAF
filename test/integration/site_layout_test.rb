fd03b7ae3733fd0e7f19770a420856f0231ef683
matteobiffoni <biffoni.1700889@studenti.uniroma1.it>
2019-06-20 17:57:57 +0200

    Filled layout

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  test "layout links" do
+    get root_path
+    assert_template 'home/home'
+    assert_select "a[href=?]", root_path
+  end
+end
